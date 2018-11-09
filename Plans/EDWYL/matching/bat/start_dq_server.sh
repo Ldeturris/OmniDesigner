63a527d18b07afc4dff2c1f25e89ae1e389db219
Ldeturris <>
2018-11-09 07:19:13 -0500

    Deleted project bundle and DQ Project for project with name EDW@v.29

--- Plans/EDWYL/matching/bat/start_dq_server.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-DQC_HOME=../../../runtime
-CLASSPATH=$DQC_HOME/lib/cif-bootstrap.jar
-
-java -Duser.timezone=UTC -Xmx1000m -cp $CLASSPATH com.ataccama.dqc.bootstrap.DqcBootstrap $DQC_HOME com.ataccama.dqc.server.bin.OnlineCtl -config=../services/_match_services.serverConfig start
